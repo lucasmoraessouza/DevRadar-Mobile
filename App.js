0f1eb315931d8c9b92f00447c4757ef63acb942e
Lucas <lucas.msouza@hotmail.com>
2021-03-25 14:40:25 -0300

    [ADD] search devs automatic

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import {StatusBar} from 'react-native'
+import React from "react";
+import { StatusBar, YellowBox } from "react-native";
 
-import Routes from './src/routes'
+import Routes from "./src/routes";
+
+YellowBox.ignoreWarnings(["Unrecognized WebSocket"]);
 
 export default function App() {
   return (
     <>
-    <StatusBar barStyle="light-content" backgroundColor="#7d40e7"/>
-    <Routes />
+      <StatusBar barStyle="light-content" backgroundColor="#7d40e7" />
+      <Routes />
     </>
   );
-};
+}
